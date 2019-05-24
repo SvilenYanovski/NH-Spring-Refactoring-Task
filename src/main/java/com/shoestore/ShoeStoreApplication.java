b6d6334346cd6d333c828f30024660fd9fb8556a
Svilen Yanovski <svilennn@yahoo.co.uk>
2019-05-24 21:59:17 +0300

    NH-3: configured the loger, moved from log4j to log4j2

--- src/main/java/com/shoestore/ShoeStoreApplication.java
+++ src/main/java/com/shoestore/ShoeStoreApplication.java
@@ -1,11 +1,16 @@
 package com.shoestore;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShoeStoreApplication {
+    private static final Logger LOGGER = LogManager.getLogger(ShoeStoreApplication.class);
+
     public static void main(String[] args) {
+        LOGGER.info("About to launch...");
         SpringApplication.run(ShoeStoreApplication.class, args);
     }
 }
